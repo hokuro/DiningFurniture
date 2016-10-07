3412c7b2d4ac57d63547e4a723df863a11645441
hokuro <ohonou@gmail.com>
2016-10-07 12:01:07 +0900

    かき氷期レンダー追加

--- src/main/java/mod/drf/foods/render/RenderFlapeMaker.java
+++ src/main/java/mod/drf/foods/render/RenderFlapeMaker.java
@@ -1,14 +1,51 @@
 package mod.drf.foods.render;
 
 import mod.drf.foods.tileentity.TileEntityFlapeMaker;
+import mod.drf.model.ModelFlape;
+import mod.drf.model.ModelFlape.EnumFlapeLevel;
+import mod.drf.model.ModelFlapeMaker;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.ResourceLocation;
 
 public class RenderFlapeMaker extends TileEntitySpecialRenderer<TileEntityFlapeMaker> {
+	private static final ResourceLocation tex = new ResourceLocation("drf:textures/entity/flapemaker.png");
+	private static final ResourceLocation tex_flape = new ResourceLocation("der:textures/entity/flape_none.png");
+
+	private ModelFlapeMaker mainModel = new ModelFlapeMaker();
+	private ModelFlape subModel = new ModelFlape();
 
 	@Override
 	public void renderTileEntityAt(TileEntityFlapeMaker te, double x, double y, double z, float partialTicks, int destroyStage) {
-		// TODO 自動生成されたメソッド・スタブ
+		renderFlapeMaker((TileEntityFlapeMaker)te,x,y,z,partialTicks,destroyStage);
 
 	}
 
+
+	public void renderFlapeMaker(TileEntityFlapeMaker te, double x, double y, double z, float partialTicks, int destroyStage) {
+		double sx,sy,sz = 0.0D;
+		double tx,ty,tz = 0.0D;
+		sx = sy = sz = 0.03125D;
+		tx = ty = tz = 0.5D;
+
+		GlStateManager.pushMatrix();
+		GlStateManager.translate((float) x + 0.5F , (float) y + 0.5F, (float) z + 0.5F);
+		GlStateManager.scale(0.625D,0.625D,0.625D);
+		GlStateManager.enableCull();
+		GlStateManager.enableRescaleNormal();
+		this.bindTexture(tex);
+		this.mainModel.render(te,0F,0F,0F,0F,0F,1F);
+		GlStateManager.popMatrix();
+
+		if (TileEntityFlapeMaker.isRunning(te)){
+			GlStateManager.pushMatrix();
+			GlStateManager.translate((float) x + 0.5F , (float) y + 0.5F, (float) z + 0.5F);
+			GlStateManager.scale(0.0125D,0.0125D,0.0125D);
+			GlStateManager.enableCull();
+			GlStateManager.enableRescaleNormal();
+			this.bindTexture(tex_flape);
+			this.subModel.render(EnumFlapeLevel.getValue(TileEntityFlapeMaker.CRUSH_TIME_MAX, te.getField(0)), 0, EnumFlapeLevel.RotationY(TileEntityFlapeMaker.CRUSH_TIME_MAX, te.getField(0)), 0, 0.0125F);
+			GlStateManager.popMatrix();
+		}
+	}
 }
--- src/main/java/mod/drf/model/ModelIcecream.java
+++ src/main/java/mod/drf/model/ModelIcecream.java
@@ -9,7 +9,11 @@
 
 
 
-package net.minecraft.src;
+package mod.drf.model;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
 
 public class ModelIcecream extends ModelBase
 {
@@ -22,12 +26,12 @@ public class ModelIcecream extends ModelBase
     ModelRenderer casecupsid2;
     ModelRenderer casecupsid1;
     ModelRenderer Ice1;
-  
+
   public ModelIcecream()
   {
     textureWidth = 64;
     textureHeight = 32;
-    
+
       casebase = new ModelRenderer(this, 0, 0);
       casebase.addBox(-4F, 0F, -4F, 8, 1, 8);
       casebase.setRotationPoint(0F, 23F, 0F);
@@ -77,7 +81,7 @@ public class ModelIcecream extends ModelBase
       Ice1.mirror = true;
       setRotation(Ice1, 0F, 0F, 0F);
   }
-  
+
   public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
   {
     super.render(entity, f, f1, f2, f3, f4, f5);
@@ -91,17 +95,17 @@ public class ModelIcecream extends ModelBase
     casecupsid1.render(f5);
     Ice1.render(f5);
   }
-  
+
   private void setRotation(ModelRenderer model, float x, float y, float z)
   {
     model.rotateAngleX = x;
     model.rotateAngleY = y;
     model.rotateAngleZ = z;
   }
-  
+
   public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
   {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5);
+    super.setRotationAngles(f, f1, f2, f3, f4, f5, null);
   }
 
 }
